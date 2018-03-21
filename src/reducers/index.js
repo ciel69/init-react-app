3f1d727fa0c5312190842ffc452d86a560125abc
Alexei <bukhaltsov@componetix.ru>
2018-03-21 21:56:54 +0300

    initial commit

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,16 +1,10 @@
 import { combineReducers } from 'redux';
-import user from './user';
-import formNewUser from './formNewUser';
-import partHistory from './partHistory';
-import formCode from './formCode';
+import reduce from './reduce';
 import { reducer as form } from 'redux-form';
 
 const reducers = {
-  user,
+  reduce,
   form, // the state and validation of form fields
-  formNewUser,
-  formCode,
-  partHistory,
 };
 
 export default combineReducers(reducers);
